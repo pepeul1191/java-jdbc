6fcd15e936b4ebf7446bfe96cb249c532d38e060
Pepe Valdivia <jvaldivia@softweb.pe>
2017-11-20 09:37:53 -0500

    CRUD con SQLITE

--- src/pe/softweb/app/App.java
+++ src/pe/softweb/app/App.java
@@ -12,14 +12,14 @@ public class App {
             //System.out.println(lista.get(i));
         }
         
-        Animal a2 = new Animal("koki", "chihuagua nerviositus", 2.01, 2);
+        //Animal a2 = new Animal("koki", "chihuagua nerviositus", 2.01, 2);
         //a2.crear();
         //System.out.println(a2);
-        //Animal a3 = new Animal(135, "Koki", "chihuagua nerviositus", 2.01, 3);
+        //Animal a3 = new Animal(132, "Koki", "chihuagua nerviositus", 2.01, 3);
         //a3.editar();
         
         Animal a3 = new Animal();
-        a3.setId(134);
+        a3.setId(132);
         a3.eliminar();
     } 
 }
--- src/pe/softweb/configs/Database.java
+++ src/pe/softweb/configs/Database.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class Database {
     //private static String url = "jdbc:mysql://localhost:3306/db_animales";
-    static final String url = "jdbc:sqlite:db/db_accesos.db";
+    static final String url = "jdbc:sqlite:db/db_animales.db";
     private static String user = "root";
     private static String password = "123";
 
@@ -15,13 +15,13 @@ public class Database {
            try {
                // 2: Cargar el driver
                // puede lanzar la excepcion ClassNotFoundException
-               Class.forName("com.sqlite.jdbc.Driver");
+               //Class.forName("com.sqlite.jdbc.Driver");
                // 3: Obtener una conexion a la BD
                // Puede lanzar un SQLException
                con = DriverManager.getConnection(url, user, password);
        
-           } catch (ClassNotFoundException ex) {
-               System.out.println("Verifica tu driver en el classpath");
+           //} catch (ClassNotFoundException ex) {
+               //System.out.println("Verifica tu driver en el classpath");
                
            } catch (SQLException ex) {
                
